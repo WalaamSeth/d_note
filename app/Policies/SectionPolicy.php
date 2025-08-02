92cd5c92dce03f9d2bf33ccb4e1fba9ba17d99b6
Walaam <theoldlightkeeper@mail.ru>
2025-08-02 10:25:55 +0500

    two commit(section_policy)

---
+++ app/Policies/SectionPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Section;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class SectionPolicy
+{
+    public function view(User $user, Section $section)
+    {
+        return $user->id === $section->user_id;
+    }
+
+    public function update(User $user, Section $section)
+    {
+        return $user->id === $section->user_id;
+    }
+
+    public function delete(User $user, Section $section)
+    {
+        return $user->id === $section->user_id;
+    }
+}
